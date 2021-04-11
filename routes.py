98173f725f0375772ce54c05747d3dbeabce2596
Ashutosh Verma <ashutoshv1520@gmail.com>
2021-04-11 12:32:20 +0530

    data showing on site backend

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from app import app,gsheet
-from flask import jsonify, request, abort,render_template
+from flask import jsonify, request, abort,render_template, url_for,json
+import os
 import re
 import json
 
@@ -12,7 +13,12 @@ def test():
 
 @app.route('/feed.html')
 def feed():
-    return render_template('feed.html')
+    #return render_template('feed.html')
+
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static/data", "Names1.json")
+    data = json.load(open(json_url))
+    return render_template('feed.html', dataz=data)
 
 
 @app.route('/')
