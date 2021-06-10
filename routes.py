3f182220ca024716286967b7730097d01007e6d1
Ashutosh Verma <ashutoshv1520@gmail.com>
2021-06-10 22:44:37 +0530

    minor changes

--- routes.py
+++ routes.py
@@ -5,30 +5,53 @@ import re
 import json
 
 
-@app.route('/test', methods=["GET"])
-def test():
-    x=str(gsheet.get_all_records())
-    z=gsheet.get_all_records()
-    return render_template('test1.html',dataz=z)
+# @app.route('/test', methods=["GET"])
+# def test():
+#     x=str(gsheet.get_all_records())
+#     z=gsheet.get_all_records()
+#     return render_template('test1.html',dataz=z)
 
 
-@app.route('/tabletest')
+@app.route('/test.html')
 def tabletest():
     #x=str(gsheet.get_all_records())
     #z=gsheet.get_all_records()
-    return render_template('index2.html')
+    return render_template('test.html')
 
 
+@app.route('/crime-charts.html')
+def crime_charts():
+    return render_template('crime-charts.html')
 
+
+@app.route('/crime-locator.html')
+def crime_locator():
+    return render_template('crime-locator.html')
+
+
+@app.route('/crime-predictor.html')
+def crime_predictor():
+    return render_template('crime-predictor.html')
 
 @app.route('/feed.html')
 def feed():
-    #return render_template('feed.html')
+    return render_template('feed.html')
 
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "static/data", "Names1.json")
-    data = json.load(open(json_url))
-    return render_template('feed.html', dataz=data)
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+
+# @app.route('/feed.html')
+# def feed():
+#     #return render_template('feed.html')
+
+#     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+#     json_url = os.path.join(SITE_ROOT, "static/data", "Names1.json")
+#     data = json.load(open(json_url))
+#     return render_template('feed.html', dataz=data)
 
 
 @app.route('/')
