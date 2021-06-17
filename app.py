6798ca51dfc96a989385b33ed3996ca757050b2e
Ashutosh Verma <ashutoshv1520@gmail.com>
2021-06-17 17:45:57 +0530

    minor changes

--- app.py
+++ app.py
@@ -21,5 +21,7 @@ from routes import *
 # runs app
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
-    app.run(debug=True,host='0.0.0.0')
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', debug=False)
+    
 print("done")
