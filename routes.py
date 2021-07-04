ef455c6ac2bbc9ca8ae3e5581b90f0d5ae482b91
Ashutosh Verma <ashutoshv1520@gmail.com>
2021-07-04 20:14:36 +0530

    test

--- routes.py
+++ routes.py
@@ -1,28 +1,9 @@
 from app import app
-#,gsheet
 from flask import jsonify, request, abort,render_template, url_for,json
 import flask
 import os
 import re
 import json
-
-
-
-
-
-
-# @app.route('/test', methods=["GET"])
-# def test():
-#     x=str(gsheet.get_all_records())
-#     z=gsheet.get_all_records()
-#     return render_template('test1.html',dataz=z)
-
-
-
-# @app.route('/hello/<user>')
-# def hello_name(user):
-    
-#     return render_template('hello.html', name = user)
 
 
 @app.route('/crime-charts.html')
@@ -71,17 +52,12 @@ def helppage():
 
 
 from webscapper import *
-
 @app.route('/',methods=['GET','POST'])
 @app.route('/index.html')
 def index():
     request_method=request.method
     if request.method=='POST':
-        print("post chala rha huun")
         webscrappingfun()
-    #return render_template('mapper.html')
     return render_template('index.html')
-    #return app.send_static_file('index.html')
-    #return 'Hello World!'
 
     
